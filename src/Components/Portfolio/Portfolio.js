74bf8621645d9511c1592d0013f5f614b112dbab
jason-hannett <jason.hannett@outlook.com>
2020-09-15 21:59:44 -0600

    crossing finger

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -29,7 +29,7 @@ function Portfolio(props){
                 </div>
             </div>
             <div className='the-drop-container'>
-                <div className='the-drop-specs-container'>
+                <div >
                     <h2>The Drop Podcast</h2>
                     <p>technologies</p>
                         <ul>
